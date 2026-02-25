fd08ff13dd2361d54ce4ebc4e9d9481dafeaf1a1
abhinavxshrivastav <abhinavkumar8106@gmail.com>
2026-02-25 10:45:53 +0530

    UC2: for code

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,2 +1,17 @@
 public class PalindromeCheckerApp {
+    static void main(String[] args) {
+        String str ="madam";
+        boolean isPalindrome = true;
+        for (int i =0; i< str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length()-1-i)){
+                isPalindrome=false;
+                break;
+            }
+        }
+        if(isPalindrome){
+            System.out.println(str+ "is Palindrome");
+        }else{
+            System.out.println(str+ "is not Palindrome");
+        }
+    }
 }
